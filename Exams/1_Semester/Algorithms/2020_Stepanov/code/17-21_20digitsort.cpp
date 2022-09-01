e5af75401ff2d630525394c57dad45c4833079ab
Daniil Dryabin <daniild71r@gmail.com>
2022-09-01 17:53:16 +0300

    Revert "files restored"
    This reverts commit 630852b0589cced1b3dac4bd380cdd43cd512f6d.

--- Exams/1_Semester/Algorithms/2020_Stepanov/code/17-21_20digitsort.cpp
+++
@@ -1,6 +0,0 @@
-void CountingSort(long long* a, int n, int byte);
-void LSDSort(long long* a, int n) {
-    for (int r = 0; r < sizeof(long long); ++r) {
-        CountingSort(a, n, r);
-    }
-}
--- Exams/1_Semester/Algorithms/2020_Stepanov/code/17-21_detstablesort.cpp
+++
@@ -1,4 +0,0 @@
-KStatLin(a, k) \\поиск k-ой проядковой статистики в массиве a
-
-x = KStatLin(a, (n + 1)/2)
-Partition(a, x)
